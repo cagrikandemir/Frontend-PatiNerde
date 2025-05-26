a73b1347c6ca2ad434523273523d3ed978ddc752
Çağrı <cagrikandemir58@icloud.com>
2025-05-26 15:47:54 +0300

    Navbar
    Tailwind Css kullanarak Responsive Navbar oluşturuldu

--- patiner/src/Components/SideBar.jsx
+++ patiner/src/Components/SideBar.jsx
@@ -1,55 +1,76 @@
-import logo from '../Image/logo.png'
+import React, { useState } from 'react'
+import logo from '../Image/logowhite.png';
+
+// icons
 import { MdMenuOpen } from "react-icons/md";
-import { FaHome } from "react-icons/fa";
-import { FaMapLocationDot } from "react-icons/fa6";
-import { FaCircleUser } from "react-icons/fa6";
-
-
-
+import { IoHomeOutline } from "react-icons/io5";
+import { FaUserCircle } from "react-icons/fa";
+import { TbReportSearch } from "react-icons/tb";
+import { CiSettings } from "react-icons/ci";
+import { FaMapMarkedAlt } from "react-icons/fa";
 
 const menuItems = [
   {
-    icons: <FaHome size={25} />,
-    label: 'Home',
+    icons: <IoHomeOutline size={30} />,
+    label: 'Home'
   },
   {
-    icons: <FaMapLocationDot size={25} />,
-    label: 'Harita',
+    icons: <FaMapMarkedAlt size={30} />,
+    label: 'Map'
   },
+
+  {
+    icons: <TbReportSearch size={30} />,
+    label: 'Report'
+  },
+  {
+    icons: <CiSettings size={30} />,
+    label: 'Setting'
+  }
 ]
-const SideBar = () => {
+
+export default function Sidebar() {
+
+  const [open, setOpen] = useState(true)
+
   return (
-    <nav className='shadow-md h-screen w-60 p-2 flex flex-col'>
-      <div className='border px-3 py-2 h-20 flex justify-between items-center'>
-        <img src={logo} alt="Logo" className='w-20 rounded-out' />
-        <MdMenuOpen size={35} className='cursor-pointer' />
+    <nav className={`shadow-md h-screen p-2 flex flex-col duration-500 bg-purple-500 text-white ${open ? 'w-60' : 'w-16'}`}>
+
+      
+      <div className=' px-3 py-2 h-20 flex justify-between items-center'>
+        <img src={logo} alt="Logo" className={`${open ? 'w-20' : 'w-0'} rounded-md`} />
+        <div><MdMenuOpen size={34} className={` duration-500 cursor-pointer ${!open && ' rotate-180'}`} onClick={() => setOpen(!open)} /></div>
       </div>
 
+      
 
       <ul className='flex-1'>
         {
           menuItems.map((item, index) => {
             return (
-              <li className='px-3 py-2 my-2 hover:bg-blue-100 rounded-md duration-300 cursor-pointer flex gap-2 items-center' key={index}>
+              <li key={index} className='px-3 py-2 my-2 hover:bg-purple-400 rounded-md duration-300 cursor-pointer flex gap-2 items-center relative group'>
                 <div>{item.icons}</div>
-                <p>{item.label}</p>
+                <p className={`${!open && 'w-0 translate-x-24'} duration-500 overflow-hidden`}>{item.label}</p>
+                <p className={`${open && 'hidden'} absolute left-32 shadow-md rounded-md
+                 w-0 p-0 text-black bg-white duration-100 overflow-hidden group-hover:w-fit group-hover:p-2 group-hover:left-16
+                `}>{item.label}</p>
               </li>
             )
-          }
-          )}
+          })
+        }
       </ul>
+      
 
-
-      <div className=' flex items-center gap-2 px-3 py-2'>
-        <FaCircleUser size={60} />
-        <div  className='leading-5'>
-          <p>Çağrı Kandemir</p>
+      <div className='flex items-center gap-2 px-3 py-2'>
+        <div><FaUserCircle size={30} /></div>
+        <div className={`leading-5 ${!open && 'w-0 translate-x-24'} duration-500 overflow-hidden`}>
+          <p>Çağrı</p>
           <span className='text-xs'>cagrikandemir.dev@gmail.com</span>
+
         </div>
       </div>
-    </nav>
 
+
+    </nav>
   )
 }
-
-export default SideBar
